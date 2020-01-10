288bb418c7f27231ec0b58054a90faad1779e690
shota.koguchi <tetris.2000@gmail.com>
2020-01-11 00:44:40 +0900

    masonry

---
+++ app/assets/javascripts/main/masonry.js
@@ -0,0 +1,12 @@
+$(document).on('turbolinks:load', function(){
+  const $posts_list = $('#posts_list');  //コンテナとなる要素を指定
+  
+  $posts_list.imagesLoaded(function(){   //imagesLoadedを使用し、画像が読み込みまれた段階でMasonryの関数を実行させる
+    //Masonryの関数↓
+    $posts_list.masonry({      //オプション指定箇所
+      itemSelector: '.post_item',  //コンテンツを指定
+      columnWidth: 205,    //カラム幅を設定
+      fitWidth: true,          //コンテンツ数に合わせ親の幅を自動調整
+    });
+  });
+});
