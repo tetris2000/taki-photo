8496f15e20d4cf944dffa1aeed06979f823a3bc3
shota.koguchi <tetris.2000@gmail.com>
2020-01-13 17:17:04 +0900

    icon preview

--- app/assets/javascripts/main/preview.js
+++ app/assets/javascripts/main/preview.js
@@ -1,5 +1,5 @@
 $(document).on("turbolinks:load", function(){
-  $fileField = $('#preview')
+  $fileField = $('#post_preview')
  
   $($fileField).on('change', $fileField, function(e) {
     file = e.target.files[0]
@@ -12,7 +12,31 @@ $(document).on("turbolinks:load", function(){
         $preview.append($('<img>').attr({
           src: e.target.result,
           width: "100%",
-          class: "preview",
+          class: "post_preview",
+          title: file.name
+        }));
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  });
+});
+
+
+$(document).on("turbolinks:load", function(){
+  $fileField = $('#icon_preview')
+ 
+  $($fileField).on('change', $fileField, function(e) {
+    file = e.target.files[0]
+    reader = new FileReader(),
+    $preview = $("#img_field");
+ 
+    reader.onload = (function(file) {
+      return function(e) {
+        $preview.empty();
+        $preview.append($('<img>').attr({
+          src: e.target.result,
+          width: "200",
+          class: "icon_preview",
           title: file.name
         }));
       };
